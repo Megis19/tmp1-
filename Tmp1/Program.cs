15593a07edb08137491ad6723e961476fca04f25
Megis19 <igorvaskan1917@gmail.com>
2021-11-12 16:31:01 +0300

    first commit

---
+++ Tmp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tmp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 0;
+
+            Console.WriteLine("Hello Git!");
+        }
+    }
+}
